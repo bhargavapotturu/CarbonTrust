99bdf5a08d24b309ad6980444edd52c230d45ea0
bhargavapotturu <bhargava.potturu@gmail.com>
2026-02-27 12:57:06 -0500

    Fixed empty map box in frontend

--- frontend/src/components/ForestForm.jsx
+++ frontend/src/components/ForestForm.jsx
@@ -78,8 +78,8 @@ function BboxMap({ minLon, minLat, maxLon, maxLat }) {
         attributionControl={false}
       >
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/dark_matter/{z}/{x}/{y}{r}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> &copy; <a href="https://carto.com/">CARTO</a>'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; OpenStreetMap contributors'
         />
         <Rectangle
           bounds={bounds}
